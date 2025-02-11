7bb4692a3363fb32205669164ff7698252033c28
sofffja <sofi.efron@gmail.com>
2025-02-11 16:09:54 +0000

    Add transmission types fetching and display in Transmissions component

--- src/components/Transmissions.jsx
+++ src/components/Transmissions.jsx
@@ -1,16 +1,36 @@
-import { usePastTransmissions } from "../hooks/useData";
+import { usePastTransmissions, useTransmissionTypes } from "../hooks/useData";
 import SectionTitle from "./SectionTitle";
 
-export default function Programs() {
-  const { data, isLoading, error } = usePastTransmissions();
+export default function Transmissions() {
+  const {
+    data: transmissions,
+    isLoading: transmissionsLoading,
+    error: transmissionsError,
+  } = usePastTransmissions();
 
-  if (isLoading) return <div>Cargando...</div>;
-  if (error) return <div>Hubo un error al cargar los programas</div>;
+  const {
+    data: types,
+    isLoading: typesLoading,
+    error: typesError,
+  } = useTransmissionTypes();
+
+  if (transmissionsLoading || typesLoading) return <div>Cargando...</div>;
+  if (transmissionsError || typesError)
+    return <div>Hubo un error al cargar los programas</div>;
 
   return (
     <div className="flex flex-col gap-4 border-b p-4">
       <SectionTitle title="Transmisiones pasadas" />
-      {data.map((transmission) => (
+
+      <div className="flex flex-wrap gap-2">
+        {types.map((type) => (
+          <span key={type._id} className="border px-2 text-xs lowercase">
+            {type.tipoDeTransmision && type.tipoDeTransmision}
+          </span>
+        ))}
+      </div>
+
+      {transmissions.map((transmission) => (
         <div key={transmission._id}>
           {transmission.titulo && transmission.titulo}
         </div>
