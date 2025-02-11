d89f533417eb5996052d4abf51b21b046101df48
astrosuka <yegresvoselok@gmail.com>
2025-02-11 00:18:40 -0300

    Add functions to fetch future transmissions, initial data, tags, and transmission types

--- src/sanity/sanity-utils.js
+++ src/sanity/sanity-utils.js
@@ -1,5 +1,59 @@
 import { client } from "./client";
 
-// export async function getPosts() {
-//   return client.fetch(`*[_type == "post"] | order(date desc) { ... }`);
-// }
+export async function getFutureTransmissions() {
+  return client.fetch(`*[_type == "transmision" && fecha > now()] | order(fecha asc) { 
+    _id,
+    titulo,
+    fecha,
+    tipoDeTransmision[]->{
+      _id,
+      tipoDeTransmision
+    },
+    programa->{
+      titulo},
+    contexto->{
+      titulo},
+    descripcionCorta,
+  }`);
+}
+
+export async function getInitialData() {
+  return client.fetch(
+    `*[_type == "infoGeneral"][0] {
+      titulo,
+      descripcion,
+      contacto,
+      redesSociales,
+      logo {
+        asset->{
+          url
+        },
+      },
+      configuracionDeTransmision,
+      destacados[]->{
+        _id,
+        _type,
+        titulo,
+        slug,
+      },
+    }`,
+  );
+}
+
+export async function getTags() {
+  return client.fetch(
+    `*[_type == "tag"] {
+      _id,
+      tag,
+    }`,
+  );
+}
+
+export async function getTransmissionTypes() {
+  return client.fetch(
+    `*[_type == "tipoDeTransmision"] {
+      _id,
+      tipoDeTransmision,
+    }`,
+  );
+}
