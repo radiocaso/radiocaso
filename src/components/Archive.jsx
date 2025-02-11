4d2fd9d410dcc90ed0fd7cd7957954ddb960d015
astrosuka <yegresvoselok@gmail.com>
2025-02-11 02:43:33 -0300

    Remove SectionTitle component from Archive

--- src/components/Archive.jsx
+++ src/components/Archive.jsx
@@ -1,4 +1,3 @@
-import SectionTitle from "./SectionTitle";
 import Contexts from "./Contexts";
 import Programs from "./Programs";
 import Transmissions from "./Transmissions";
@@ -6,7 +5,6 @@ import Transmissions from "./Transmissions";
 export default function Archive() {
   return (
     <>
-      <SectionTitle title="Archivo" />
       <Contexts />
       <Programs />
       <Transmissions />
