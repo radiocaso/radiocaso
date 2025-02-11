2192f44c86842bd4f4c3a4ed3f0ed57f2b5ecb96
astrosuka <yegresvoselok@gmail.com>
2025-02-11 00:26:08 -0300

    Add Home component to integrate FutureTransmissions and Highlights

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import FutureTransmissions from "./FutureTransmissions";
+import Highlights from "./Highlights";
+
+export default function Home() {
+  return (
+    <>
+      <FutureTransmissions />
+      <Highlights />
+    </>
+  );
+}
